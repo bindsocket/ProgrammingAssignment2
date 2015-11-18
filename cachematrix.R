df1122f0cfd4c772d988b9b1b5b810d7baca10c6
Brian Werner <bindsocket@gmail.com>
2015-11-18 18:34:59 -0500

    Working I believe

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <<- NULL
+  set <- function(y) {
+     xm <<- y
+     inv <<- NULL
+  }
+  get <- function() {xm}
+  setinv <- function(xinv) {inv<<-xinv}
+  getinv <- function() {inv}
+  list(set = set,get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  xinv <- x$getinv()
+  if (!is.null(xinv)) {
+    message("retrieving cached value...")
+    return(xinv)
+  }
+  v_data <- x$get()
+  v_inv <- solve(v_data)
+  x$setinv(v_inv)
+  return(v_inv)
 }
